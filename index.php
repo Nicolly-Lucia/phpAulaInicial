b6d8fccb9e26fda77979a4558ca771dea2117cf7
Nicolly Lucia Da Silva <nicklucia0401@gmail.com>
2026-02-26 17:55:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 2</title>
+</head>
+<body>
+    <?php
+    echo "Olá mundo que usa o PHP, ESSE É MEU PRIMEIRO PROGRAMA!";
+        echo "Espero aprender bastante sobre essa poderosa linguagem";
+    echo "durante as aulas";
+
+    ?>
+
+</body>
+</html>
