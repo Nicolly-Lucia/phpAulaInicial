a08803fc781522d8541e94c98922ecb4a7609f9a
Nicolly Lucia Da Silva <nicklucia0401@gmail.com>
2026-02-26 17:59:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 2</title>
 </head>
 <body>
     <?php
@@ -14,4 +11,5 @@
     ?>
 
 </body>
+
 </html>
